dd21ba4edebedf88514ce5bb216831b80674648f
Daniel Mihai <danikmihai23@mail.ru>
2024-11-18 16:40:52 +0200

    Feature: Footer block

--- app/Views/about.php
+++ app/Views/about.php
@@ -6,10 +6,14 @@
     <!-- Connecting CSS -->
     <link rel="stylesheet" href="/public/css/index.css">
     <link rel="stylesheet" href="/public/css/header.css">
+    <link rel="stylesheet" href="/public/css/footer.css">
     <title>Finstar - About Us</title>
 </head>
 <body>
     <!-- Connecting header block -->
     <?php include("layouts/header.php"); ?>
+
+    <!-- Connecting footer block -->
+    <?php include("layouts/footer.php"); ?>
 </body>
 </html>
