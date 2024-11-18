5c7f09bc77820a63f33450a422b89c952fc676be
Daniel Mihai <danikmihai23@mail.ru>
2024-11-18 16:10:08 +0200

    Feature: Header block

--- app/Views/about.php
+++ app/Views/about.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Connecting CSS -->
+    <link rel="stylesheet" href="/public/css/index.css">
+    <link rel="stylesheet" href="/public/css/header.css">
     <title>Finstar - About Us</title>
 </head>
 <body>
-    
+    <!-- Connecting header block -->
+    <?php include("layouts/header.php"); ?>
 </body>
 </html>
---
+++ app/Views/layouts/header.php
@@ -0,0 +1,17 @@
+<!-- Header block -->
+<header>
+    <img src="/public/images/logo.png" alt="Finstar">
+
+    <nav>
+        <a class="active" href="/app/Views/about.php">О компании</a>
+        <a href="">Паевые фонды</a>
+        <a href="">Раскрытие информации</a>
+        <a href="">Новости</a>
+        <a href="">Контакты</a>
+    </nav>
+
+    <div class="btns">
+        <a href="">Вход</a>
+        <a class="reg" href="">Регистрация</a>
+    </div>
+</header>
