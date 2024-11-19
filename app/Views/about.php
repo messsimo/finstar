3db38d871e855191de14462b0a94eaa5f89a6564
Daniel Mihai <danikmihai23@mail.ru>
2024-11-19 08:39:27 +0200

    Feature: Hero block

--- app/Views/about.php
+++ app/Views/about.php
@@ -7,11 +7,28 @@
     <link rel="stylesheet" href="/public/css/index.css">
     <link rel="stylesheet" href="/public/css/header.css">
     <link rel="stylesheet" href="/public/css/footer.css">
+    <link rel="stylesheet" href="/public/css/about.css">
     <title>Finstar - About Us</title>
 </head>
 <body>
     <!-- Connecting header block -->
     <?php include("layouts/header.php"); ?>
+
+    <!-- Hero block -->
+    <div class="hero">
+        <div class="page-nav">
+            <span>Главная > <b>О компании</b></span>
+        </div>
+
+        <div class="container-hero">
+            <div class="text">
+                <span>Профессиональное управление инвестициями с 2005 года</span>
+                <h2>ООО «Управляющая компания «Finstar»</h2>
+            </div>
+
+            <img src="/public/images/hero-logo.png" alt="Finstar">
+        </div>
+    </div>
 
     <!-- Connecting footer block -->
     <?php include("layouts/footer.php"); ?>
