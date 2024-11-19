e1b32f05f6a119f70c9139cbd388f44435e2c586
Daniel Mihai <danikmihai23@mail.ru>
2024-11-19 12:33:31 +0200

    Edits

--- app/Views/about.php
+++ app/Views/about.php
@@ -1,102 +1,78 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Connecting CSS -->
-    <link rel="stylesheet" href="/public/css/index.css">
-    <link rel="stylesheet" href="/public/css/header.css">
-    <link rel="stylesheet" href="/public/css/footer.css">
-    <link rel="stylesheet" href="/public/css/about.css">
-    <title>Finstar - About Us</title>
-</head>
-<body>
-    <!-- Connecting header block -->
-    <?php include("layouts/header.php"); ?>
-
-    <!-- Hero block -->
-    <div class="hero">
-        <div class="page-nav">
-            <span>Главная > <b>О компании</b></span>
-        </div>
-
-        <div class="container-hero">
-            <div class="text">
-                <span>Профессиональное управление инвестициями с 2005 года</span>
-                <h2>ООО «Управляющая компания «Finstar»</h2>
-            </div>
-
-            <img src="/public/images/hero-logo.png" alt="Finstar">
-        </div>
+<!-- Hero block -->
+<div class="hero">
+    <div class="page-nav">
+        <span>Главная > <b>О компании</s></span>
     </div>
 
-    <!-- Advantages block -->
-    <div class="advantages">
-        <div class="container-advantages">
-            <div class="block">
+    <div class="container-hero">
+        <div class="text">
+            <span>Профессиональное управление инвестициями с 2005 года</span>
+            <h2>ООО «Управляющая компания</h2>
+            <h2>«Finstar»</h2>
+        </div>
+
+        <img src="/public/images/hero-logo.png" alt="Finstar">
+    </div>
+</div>
+
+<!-- Advantages block -->
+<div class="advantages">
+    <div class="container-advantages">
+        <div class="block">
                 <h3>#1</h3>
-                <span>12 паевых инвестиционных фондов в управлении</span>
+            <span>12 паевых инвестиционных фондов в управлении</span>
+        </div>
+        <div class="block">
+            <h3>#2</h3>
+            <span>43,00 млрд ₽ — объем активов на 11.12.2023</span>
+        </div>
+        <div class="block">
+            <h3>#3</h3>
+            <span>Более 80 тысяч клиентов управляющей компании</span>
+        </div>
+    </div>
+</div>
+
+<!-- FAQ block -->
+<div class="faq">
+    <div class="container-faq">
+        <div class="img">
+            <img src="/public/images/handshake.png" alt="Finstar">
+            <div class="dots">
+                <div class="dot active"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+            </div>
+        </div>
+
+        <div class="questions">
+            <h1>Деятельность Управляющей компании</h1>
+            <div class="block active">
+                <span>Доверительное управление паевыми инвестиционными фондами</span>
             </div>
             <div class="block">
-                <h3>#2</h3>
-                <span>43,00 млрд ₽ — объем активов на 11.12.2023</span>
+                <span>Доверительное управление пенсионными резервами и пенсионными накоплениями</span>
             </div>
             <div class="block">
-                <h3>#3</h3>
-                <span>Более 80 тысяч клиентов управляющей компании</span>
+                <span>Создание фонда под параметры вашего проекта</span>
+            </div>
+            <div class="block">
+                <span>Доверительное управление активами целевых капиталов (эндаумент-фондов)</span>
+            </div>
+            <div class="block">
+                <span>Доверительное управление компенсационными фондами СРО</span>
+            </div>
+            <div class="block">
+                <span>Доверительное управление средствами жилищного обеспечения накопительно-ипотечной системы военнослужащих</span>
             </div>
         </div>
     </div>
+</div>
 
-    <!-- FAQ block -->
-    <div class="faq">
-        <div class="container-faq">
-            <div class="img">
-                <img src="/public/images/handshake.png" alt="Finstar">
-                <div class="dots">
-                    <div class="dot active"></div>
-                    <div class="dot"></div>
-                    <div class="dot"></div>
-                    <div class="dot"></div>
-                    <div class="dot"></div>
-                    <div class="dot"></div>
-                </div>
-            </div>
+<!-- Priorities block-->
 
-            <div class="questions">
-                <h1>Деятельность Управляющей компании</h1>
-                <div class="block active">
-                    <div class="bar"></div>
-                    <span>Доверительное управление паевыми инвестиционными фондами</span>
-                </div>
-                <div class="block">
-                    <div class="bar"></div>
-                    <span>Доверительное управление пенсионными резервами и пенсионными накоплениями</span>
-                </div>
-                <div class="block">
-                    <div class="bar"></div>
-                    <span>Создание фонда под параметры вашего проекта</span>
-                </div>
-                <div class="block">
-                    <div class="bar"></div>
-                    <span>Доверительное управление активами целевых капиталов (эндаумент-фондов)</span>
-                </div>
-                <div class="block">
-                    <div class="bar"></div>
-                    <span>Доверительное управление компенсационными фондами СРО</span>
-                </div>
-                <div class="block">
-                    <div class="bar"></div>
-                    <span>Доверительное управление средствами жилищного обеспечения накопительно-ипотечной системы военнослужащих</span>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Connecting footer block -->
-    <?php include("layouts/footer.php"); ?>
-
-    <!-- Connecting JS -->
-    <script src="/public/js/faqChanges.js"></script>
-</body>
-</html>
+<!-- Connecting JS -->
+ <script src="/public/js/faqChanges.js"></script>
---
+++ app/Views/layouts/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Connecting CSS -->
+    <link rel="stylesheet" href="/public/css/index.css">
+    <link rel="stylesheet" href="/public/css/header.css">
+    <link rel="stylesheet" href="/public/css/footer.css">
+    <link rel="stylesheet" href="/public/css/about.css">
+    <!-- Title -->
+    <title>Finstar</title>
+</head>
+<body>
+    <!-- Connecting header block -->
+    <?php include("header.php"); ?>
+
+    <!-- Connecting about page -->
+    <?php include($_SERVER['DOCUMENT_ROOT'] . "/app/Views/about.php"); ?>
+
+
+
+    <!-- Connecting footer block -->
+    <?php include("footer.php"); ?>
+</body>
+</html>
