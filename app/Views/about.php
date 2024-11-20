94e56bf538abb1412082918247da58df7b717fce
Daniel Mihai <danikmihai23@mail.ru>
2024-11-20 06:58:39 +0200

    Feature: Adaptation for contact block

--- app/Views/about.php
+++ app/Views/about.php
@@ -312,7 +312,7 @@
         </div>
 
         <div class="map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2063.5469085217255!2d37.541452076652796!3d55.76258597308783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTXCsDQ1JzQ1LjMiTiAzN8KwMzInMzguNSJF!5e1!3m2!1sru!2s!4v1732042323077!5m2!1sru!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2063.5469085217255!2d37.541452076652796!3d55.76258597308783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTXCsDQ1JzQ1LjMiTiAzN8KwMzInMzguNSJF!5e1!3m2!1sru!2s!4v1732042323077!5m2!1sru!2s" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
 </div>
